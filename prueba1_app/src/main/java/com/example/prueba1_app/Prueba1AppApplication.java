3c5c047a842bdf0c99e2e752ac537c5dbd2ba412
ignn4 <igguerrer@alumnos.unex.es>
2023-09-28 18:12:53 +0200

    A

---
+++ prueba1_app/src/main/java/com/example/prueba1_app/Prueba1AppApplication.java
@@ -0,0 +1,13 @@
+package com.example.prueba1_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba1AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba1AppApplication.class, args);
+	}
+
+}
---
+++ prueba1_app/src/test/java/com/example/prueba1_app/Prueba1AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba1_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba1AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
